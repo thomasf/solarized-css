5b72a1a6389ff179207a8190f195ef59567b29b2
Thomas Frössman <thomasf@jossystem.se>
2014-02-01 14:02:46 +0100

    cripple build.sh since moving to grunt

--- build.sh
+++ build.sh
@@ -2,49 +2,31 @@
 
 set -e
 
+echo deprecated, moving to grunt file
+
+exit 1
+
 sol_src=src/solarized-css
 script_src=src/script
 export PATH
 
 arg=$1
 
-build_css() {
-    ./node_modules/.bin/stylus ${sol_src}/solarized-dark.styl -o .
-    ./node_modules/.bin/stylus ${sol_src}/solarized-light.styl -o .
-    ./node_modules/.bin/uglifycss solarized-dark.css > solarized-dark.min.css
-    ./node_modules/.bin/uglifycss solarized-light.css > solarized-light.min.css
-}
-
-build_js() {
-    ./node_modules/.bin/coffee -o . -c ${script_src}/styleswitch.coffee
-    ./node_modules/.bin/coffee -o . -c ${script_src}/org-info-extension.coffee
-    cat  ${script_src}/org-info-src.js styleswitch.js org-info-extension.js > org-info.js
-    ./node_modules/.bin/uglifyjs -nc -o styleswitch.min.js styleswitch.js
-    ./node_modules/.bin/uglifyjs -nc -o org-info.min.js org-info.js
-
-}
-
-
 case $arg in
-    css-dev)
-        ./node_modules/.bin/stylus -l -w src/solarized-css/ -o .
-        ;;
-    gh-pages)
-        git diff-files --quiet || (
-            git status
-            echo "Unclean, exiting"
-            exit 1
-        )
-        git checkout gh-pages
-        git merge master -m"merge master to gh-pages"
-        pandoc index.md > index.html
-        git add index.html && git commit -am"merged master" || echo "---"
-        git checkout master
-        ;;
-
-        *)
-        # default target is regular build
-        build_css
-        build_js
-        ;;
+  css-dev)
+    ./node_modules/.bin/stylus -l -w src/solarized-css/ -o .
+    ;;
+  gh-pages)
+    #FIXME this process needs to be changed
+    git diff-files --quiet || (
+      git status
+      echo "Unclean, exiting"
+      exit 1
+    )
+    git checkout gh-pages
+    git merge master -m"merge master to gh-pages"
+    pandoc index.md > index.html
+    git add index.html && git commit -am"merged master" || echo "---"
+    git checkout master
+    ;;
 esac
